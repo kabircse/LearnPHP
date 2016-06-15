e93d2d6f1780825ff4cb7605affa66dea58f1a1b
Kabir Hossain <kabir.pust@gmail.com>
2016-06-15 14:03:57 +0600

    Update method_overloading2.php

--- method_overloading/method_overloading2.php
+++ method_overloading/method_overloading2.php
@@ -1,30 +1,30 @@
 <?php
 /*This is a method overloading, but in php we can not declare a method twice.So we have to exp:2.
 //Exp:1
-class A {
-        public function func1($var1) {
-            echo "hello from func1 with parameter as var1";
-        }
-     
-        public function func1($var1,$var2) {
-            echo "hello from func1 with parameter as var1 and var2";
-        }
-    }
-     
-    $objA = new A;
-    $objA->func1('a');
+		class A {
+		        public function func1($var1) {
+		            echo "hello from func1 with parameter as var1";
+		        }
+		     
+		        public function func1($var1,$var2) {
+		            echo "hello from func1 with parameter as var1 and var2";
+		        }
+		    }
+		     
+		    $objA = new A;
+		    $objA->func1('a');
 */
 
-//step:2
-class A {
-	public function __call($name,$parm){
-		print_r($parm);
-	}
-}
- 
-$objA = new A;
-$objA->func1(2);
-$objA->func1(2,3,4);
+//exp:2
+		class A {
+			public function __call($name,$parm){
+				print_r($parm);
+			}
+		}
+		 
+		$objA = new A;
+		$objA->func1(2);
+		$objA->func1(2,3,4);
 
 /*
 Output:
